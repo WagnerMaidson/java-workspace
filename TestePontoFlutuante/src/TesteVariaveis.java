4953893657071f9e6e61385a345165b075410fa2
wagner maidson <wagner.rmaidson@gmail.com>
2020-06-08 14:42:00 -0300

    Teste de Variaveis

---
+++ TestePontoFlutuante/src/TesteVariaveis.java
@@ -0,0 +1,18 @@
+
+public class TesteVariaveis {
+	
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("meu salario e " + salario);
+		
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5 / 2;
+		System.out.println(novaTentativa);
+	}
+}
