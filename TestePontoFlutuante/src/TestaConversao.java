2c0bb2e63b0567cc0965b408d2b3d85b77108839
wagner maidson <wagner.rmaidson@gmail.com>
2020-06-08 14:36:50 -0300

    Teste de conversao

---
+++ TestePontoFlutuante/src/TestaConversao.java
@@ -0,0 +1,16 @@
+
+public class TestaConversao {
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		
+		float pontoFlutuante = 3,14f;
+		long numeroGrande = 32432423523L;
+		shots valorPequeno = 2131;
+		byte b = 127;
+	}
+}
+
+// int (inteiro) e double (decimal)
